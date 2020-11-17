5b41c47d190687e04b384e2a6020c88270bfc7d2
Tantalus <jsl_713@live.com>
2020-11-17 11:52:21 +0800

    refact

--- realconv/conv0.cpp
+++ realconv/conv0.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <initializer_list>
 #include <vector>
+#include <chrono>
 
 
 template <typename contTy>
@@ -63,7 +64,7 @@ public:
     std::vector<dtype> data;
 
     tensor(std::initializer_list<std::size_t> args)
-        : dims(args), data(product(args)) { }
+        : dims(args), data(product(args), 0) { }
 
     template <typename DI, typename... Args>
     dtype& operator()(const DI& dimidx, Args... args) {
@@ -73,6 +74,20 @@ public:
     template <typename DI, typename... Args>
     const dtype& operator()(const DI& dimidx, Args... args) const {
         return data[dimidx.idx(args...)];
+    }
+
+    std::ifstream& readBinary(std::ifstream& is) {
+        auto ptr = &*(data.begin());
+        std::size_t size = data.size() * sizeof(dtype);
+        is.read((char*)ptr, size);
+        return is;
+    }
+
+    std::ofstream& dumpBinary(std::ofstream& os) {
+        auto ptr = &*(data.begin());
+        std::size_t size = data.size() * sizeof(dtype);
+        os.write((char*)ptr, size);
+        return os;
     }
 };
 
@@ -88,52 +103,40 @@ tensor<dtype> conv2d(const tensor<dtype> &img, const tensor<dtype> &weight) {
     assert (Ker == 3);
     tensor<dtype> ret { N, Cout, H-2, W-2 };
     DimIdx<4> dimRet(ret.dims);
-    for (int in = 0; in < N; in++) {
-        for (int co = 0; co < Cout; co++) {
-            for (int ih = 0; ih < H-2; ih++) {
-            for (int iw = 0; iw < W-2; iw++) {
-                dtype val = 0;
-                for (int ci = 0; ci < Cin; ci++) {
-                    for (int kh = 0; kh < 3; kh++) {
-                    for (int kw = 0; kw < 3; kw++) {
-                        val += weight(dimWeight, co, ci, kh, kw) * img(dimImg, in, ci, ih+kh, iw+kw);
-                    }
-                    }
-                }
-                ret(dimRet, in, co, ih, iw) = val;
-            }
-            }
-        }
+    for (int in = 0; in < N; in++)
+    for (int co = 0; co < Cout; co++)
+    for (int ih = 0; ih < H-2; ih++)
+    for (int iw = 0; iw < W-2; iw++)
+    {
+        dtype val = 0;
+        for (int ci = 0; ci < Cin; ci++)
+        for (int kh = 0; kh < Ker; kh++)
+        for (int kw = 0; kw < Ker; kw++)
+             val += weight(dimWeight, co, ci, kh, kw)
+                  * img(dimImg, in, ci, ih+kh, iw+kw);
+        ret(dimRet, in, co, ih, iw) += val;
     }
     return ret;
 }
 
 
-template <typename dtype>
-std::ifstream& readBinary(tensor<dtype> &arg, std::ifstream& is) {
-    auto ptr = &*(arg.data.begin());
-    std::size_t size = arg.data.size() * sizeof(dtype);
-    is.read((char*)ptr, size);
-    return is;
-}
-
-template <typename dtype>
-void dumpBinary(const tensor<dtype> &arg, std::ofstream& os) {
-    auto ptr = &*(arg.data.begin());
-    std::size_t size = arg.data.size() * sizeof(dtype);
-    os.write((char*)ptr, size);
-}
-
-
 int main() {
-    tensor<> weight {64, 3, 3, 3}, img {20, 3, 1000, 2000};
+    tensor<float> weight {64, 3, 3, 3}, img {20, 3, 1000, 2000};
     std::ifstream fin;
     fin.open("weight.dat", std::ifstream::binary);
-    readBinary(weight, fin).close();
+    weight.readBinary(fin).close();
     fin.open("input.dat", std::ifstream::binary);
-    readBinary(img, fin).close();
+    img.readBinary(fin).close();
+
+    using namespace std::chrono;
+    std::cout << "start" << std::endl;
+    auto t0 = steady_clock::now();
     auto result = conv2d(img, weight);
+    auto t1 = steady_clock::now();
+    auto span = duration_cast<duration<double> >(t1 - t0);
+    std::cout << "time: " << span.count() << std::endl;
+
     std::ofstream ofs("output2.dat", std::ofstream::binary);
-    dumpBinary(result, ofs);
+    result.dumpBinary(ofs);
     return 0;
 }
